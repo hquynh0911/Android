50d6e03edd6cb694be302d6f09b0f7162dfce0f3
quynh0911 <phamthihuongquynh.toan@gmail.com>
2021-05-15 17:39:20 +0700

    fixed

--- app/src/main/java/vn/edu/hust/soict/android/scorekeeper/MainActivity.java
+++ app/src/main/java/vn/edu/hust/soict/android/scorekeeper/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId()==R.id.night_mode) {
             // Get the night mode state of the app.
-            int nightMode =MaterialC.getDefaultNightMode();
+            int nightMode =AppCompatDelegate.getDefaultNightMode();
             //Set the theme mode for the restarted activity
             if(nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
